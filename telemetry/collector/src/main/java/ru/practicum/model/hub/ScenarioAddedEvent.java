e1816b813dafaabfcd7c5f01b8a1f233ef474875
Степан <bokarevstepan1@yandex.ru>
2024-11-15 17:07:14 +0300

    добавить not empty

--- telemetry/collector/src/main/java/ru/practicum/model/hub/ScenarioAddedEvent.java
+++ telemetry/collector/src/main/java/ru/practicum/model/hub/ScenarioAddedEvent.java
@@ -1,6 +1,7 @@
 package ru.practicum.model.hub;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -20,9 +21,9 @@ public class ScenarioAddedEvent extends HubEvent {
     @NotBlank
     @Size(min = 3)
     String name;
-    @NotNull
+    @NotEmpty
     List<ScenarioCondition> conditions;
-    @NotNull
+    @NotEmpty
     List<DeviceAction> actions;
 
     @Override
